6a14fa88b65aff10b8576dd2b481cede92ba42ee
akihiro-i <inoue.akihiro.ib5@naist.ac.jp>
2024-07-14 10:18:48 +0900

    サインイン変更

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -46,7 +46,7 @@ export default function SignIn() {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 10,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
